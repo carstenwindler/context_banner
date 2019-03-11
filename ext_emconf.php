2647c4995e0e0832bb02beaa4378d2d5011aee36
Carsten Windler <carsten@carstenwindler.de>
2019-03-11 22:56:04 +0100

    Fixed ext_emconf dammit

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '9.5.0-8.7.99',
+      'typo3' => '9.5.0-9.7.99',
     ),
     'conflicts' => 
     array (
