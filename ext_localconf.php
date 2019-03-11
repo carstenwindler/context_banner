4be4314ef002d8a4f4d9ede26e07058d68f9781c
Carsten Windler <carsten@carstenwindler.de>
2019-03-11 22:42:28 +0100

    Remove old-school hooks for frontend banner to at least have backend banner

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,9 +13,3 @@ if (TYPO3_MODE === 'BE') {
 
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][] = \CarstenWindler\ContextBanner\Backend\ToolbarItems\ApplicationContextToolbarItem::class;
 }
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output']['context_banner']
-    = 'EXT:context_banner/Classes/Main.php:&CarstenWindler\\ContextBanner\\Main->contentPostProcOutputHook';
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/backend.php']['renderPreProcess']['context_banner']
-    = 'EXT:context_banner/Classes/Main.php:&CarstenWindler\\ContextBanner\\Main->backendRenderPreProcessHook';
